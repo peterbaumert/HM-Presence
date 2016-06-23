d17e4768baacad6e65cea22e75e255e83df4c1fa
peterbaumert <peterbaumert@users.noreply.github.com>
2016-06-23 10:43:03 +0200

    Update presence.tcl

--- presencebymac/addon/presence.tcl
+++ presencebymac/addon/presence.tcl
@@ -1,38 +1,46 @@
 #!/bin/tclsh
 
 load tclrega.so
-source functions.tcl
 
 set host "127.0.0.1"
 set people ""
 set presence_id ""
-set FILENAME "/etc/config/addons/presence/presence.conf"
+set module ""
+set PATH "/etc/config/addons/presence"
+set FILENAME "${PATH}/presence.conf"
+
+source ${PATH}/functions.tcl
 
 proc loadConfigFile { } {
-        global FILENAME host people presence_id
+        global FILENAME host people presence_id module
 
         set content(HOST) $host
         set content(PEOPLE) $people
         set content(PRESENCE_ID) $presence_id
+        set content(MODULE) $module
         catch { array set content [loadFile $FILENAME] }
 
         set host $content(HOST)
         set people $content(PEOPLE)
         set presence_id $content(PRESENCE_ID)
+        set module $content(MODULE)
 
         debug 1 "Host: $host"
         debug 1 "People: $people"
         debug 1 "ID: $presence_id"
+        debug 1 "Module: $module"
 }
 
 proc main { } {
-        global host people presence_id
+        global PATH host people presence_id module
 
         loadConfigFile
 
-        if {$host == "127.0.0.1" || $people == "" || $presence_id == ""} {
+        if {$host == "127.0.0.1" || $people == "" || $presence_id == "" || $module == ""} {
                 error "Config not complete"
         }
+
+        source ${PATH}/modules/${module}.tcl
 
         if {![file exists /tmp/presence.state]} {
                 touch /tmp/presence.state
@@ -42,34 +50,14 @@ proc main { } {
 
         debug 1 "Prev: $prev"
 
-        set fb [exec wget -q -O - http://${host}/wlan/wlan_settings.lua]
-        regsub -all {\n} $fb "" fb
-
-        debug 2 "FB: $fb"
-
-        set macs ""
-
-        for {set x 0} {$x>=0} {incr x} {
-                if {[info exists indices]} {
-                        unset indices
-                }
-                regexp -line -indices {MAC-Adresse">(([0-9A-F]{1,2}:){5}[0-9A-F]{1,2})<\/td><td class="hint" datalabel="Datenrate">[0-9]} $fb indices
-                if {[info exists indices]} {
-                        regexp -line {MAC-Adresse">(([0-9A-F]{1,2}:){5}[0-9A-F]{1,2})<\/td><td class="hint" datalabel="Datenrate">[0-9]} $fb -> mac
-                        append macs "$mac\n"
-                        set pos [lindex $indices 1]
-                        set fb [string range $fb $pos end]
-                } else {
-                        break
-                }
-        }
+        set macs [getMacs]
 
         debug 1 "Macs: $macs"
 
         set home 0
 
         foreach {name mac} $people {
-                if {[string match *$mac* $macs]} {
+                if {[string match *[string toupper $mac]* [string toupper $macs]]} {
                         set home 1
                         debug 1 "$name with $mac is home"
                         break
